571ccddd3d385214f244910abba0a9f5f0d98eae
Batsatsugo <corajianchristian@gmail.com>
2025-05-30 03:32:14 +0800

    adding pseudocode before the define and initialze file name

--- conversion_of_assignment_9_and_10_to_oop.py
+++ conversion_of_assignment_9_and_10_to_oop.py
@@ -1,5 +1,6 @@
 import json
 
-# Define a class to encapsulate the quiz creation functionality
+# define a class to encapsulate the quiz creation functionality
 class QuizCreator:
+    # defining and initializing filename to save the quiz data
     def __init__(self, filename="the_ultimate_quiz_data.txt"):
