536cce259742192fb41c48815ba30bb7ec5f0e98
Batsatsugo <corajianchristian@gmail.com>
2025-05-30 04:22:02 +0800

    defining and initializing the filename and prepare score and question list

--- conversion_of_assignment_10_to_oop.py
+++ conversion_of_assignment_10_to_oop.py
@@ -2,3 +2,5 @@ import json
 
 # define a class to encapsulate all functionality related to running the quiz
 class QuizGame:
+    # constructor method to initialize the filename and prepare score and question list
+    def __init__(self, filename="the_ultimate_quiz_data.txt"):
