ce5b1d9be2b86b1d3a7fabda79f1b43452b5ba77
Batsatsugo <corajianchristian@gmail.com>
2025-05-30 03:34:36 +0800

    running the quiz creator

--- conversion_of_assignment_9_and_10_to_oop.py
+++ conversion_of_assignment_9_and_10_to_oop.py
@@ -4,3 +4,6 @@ import json
 class QuizCreator:
     # defining and initializing filename to save the quiz data
     def __init__(self, filename="the_ultimate_quiz_data.txt"):
+        self.filename = filename
+
+    def run(self):
