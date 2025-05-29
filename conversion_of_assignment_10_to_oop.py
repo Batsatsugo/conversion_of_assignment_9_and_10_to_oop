1cee6a9155a71454e2c2ea1ac5bcd69203b6e72b
Batsatsugo <corajianchristian@gmail.com>
2025-05-30 04:26:00 +0800

    intializing instance variable

--- conversion_of_assignment_10_to_oop.py
+++ conversion_of_assignment_10_to_oop.py
@@ -4,3 +4,6 @@ import json
 class QuizGame:
     # constructor method to initialize the filename and prepare score and question list
     def __init__(self, filename="the_ultimate_quiz_data.txt"):
+        self.filename = filename    # File where quiz questions are stored
+        self.score = 0              # Initialize score to 0
+        self.questions = []         # List to store all loaded quiz questions
